96d4cad8e48714dd8380cad14f63ffce8e4af1d6
sandra juarez <drasan_mar@hotmail.com>
2018-03-06 01:31:48 -0600

    Avance dia lunes

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tecMonterreyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tecMonterreyApp
+ */
+angular.module('tecMonterreyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tecMonterreyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tecMonterreyApp
+ */
+angular.module('tecMonterreyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
